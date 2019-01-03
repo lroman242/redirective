a6f9733eb86048fbe827876fb1652e2270b0c330
Roman <lroman242@gmail.com>
2019-01-03 22:03:51 +0200

    add common web server, add chrom trace api route

--- main.go
+++ main.go
@@ -1,172 +1,61 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
+	"github.com/lroman242/redirective/response"
 	"github.com/lroman242/redirective/tracer"
 	"github.com/raff/godet"
-	"io/ioutil"
 	"log"
+	"net/http"
 	"net/url"
 )
 
 func main() {
-	// connect to Chrome instance
-	remote, err := godet.Connect("localhost:9222", false)
+	http.HandleFunc("/api/trace/chrome", func(w http.ResponseWriter, r *http.Request) {
+		// connect to Chrome instance
+		remote, err := godet.Connect("localhost:9222", false)
+		if err != nil {
+			(&response.Response{false, fmt.Sprintf("cannot connect to Chrome instance: %s", err), 500, nil}).Failed(w)
+			return
+		}
+
+		defer func() {
+			err = remote.Close()
+			if err != nil {
+				log.Printf("remote.Close error: %s", err)
+			}
+		}()
+
+		// create new tracer instance
+		chr := tracer.NewChromeTracer(remote)
+
+		// check url
+		urlToTrace := r.URL.Query().Get("url")
+		if urlToTrace == "" {
+			(&response.Response{false, "url parameter is required", 400, nil}).Failed(w)
+			return
+		}
+		//TODO: validate url
+
+		// convert raw url string to url.URL
+		targetUrl, err := url.Parse(urlToTrace)
+		if err != nil {
+			(&response.Response{false, fmt.Sprintf("invalid url %s", err), 400, nil}).Failed(w)
+			return
+		}
+
+		// process tracing
+		redirects, err := chr.GetTrace(targetUrl)
+		if err != nil {
+			(&response.Response{false, fmt.Sprintf("sorry, an error occurred. %s", err), 500, nil}).Failed(w)
+			return
+		}
+
+		(&response.Response{true, "url successfully traced", 200, tracer.NewJSONRedirects(redirects)}).Success(w)
+	})
+
+	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
-		fmt.Println("cannot connect to Chrome instance:", err)
-		return
+		log.Printf("ListenAndServe error: %s", err)
 	}
-	defer remote.Close()
-
-	chr := tracer.NewChromeTracer(remote)
-
-	targetUrl, _ := url.Parse("https://www.google.com.ua")
-	//targetUrl, _ := url.Parse("http://compre.vc/v2/331179766c6")
-	//targetUrl, _ := url.Parse("http://trk.indoleads.com/5a435e1bb6920d3422293737")
-	//targetUrl, _ := url.Parse("http://trk.indoleads.com/5ad61882010d9")
-	redirects, err := chr.GetTrace(targetUrl)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	b, err := json.Marshal(redirects)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	ioutil.WriteFile("./result.json", b, 0644)
-	//
-	//var mainFrameId string
-	//
-	//// connect to Chrome instance
-	//remote, err := godet.Connect("localhost:9222", false)
-	//if err != nil {
-	//	fmt.Println("cannot connect to Chrome instance:", err)
-	//	return
-	//}
-	//
-	//// disconnect when done
-	//defer remote.Close()
-	//
-	//remote.EnableRequestInterception(true)
-	//
-	//// get list of open tabs
-	//tabs, _ := remote.TabList("")
-	//fmt.Println(tabs)
-	//
-	//// install some callbacks
-	//remote.CallbackEvent(godet.EventClosed, func(params godet.Params) {
-	//	fmt.Println("RemoteDebugger connection terminated.")
-	//})
-	//
-	//i := 0
-	//remote.CallbackEvent("Network.requestWillBeSent", func(params godet.Params) {
-	//	if _, ok := params["redirectResponse"]; ok && params["type"] == "Document" {
-	//		fmt.Printf("Document URL: %s\n", params["documentURL"])
-	//
-	//		b, err := json.Marshal(params)
-	//		if err != nil {
-	//			fmt.Println(err)
-	//			return
-	//		}
-	//		ioutil.WriteFile(fmt.Sprintf("./%d-%s.json", i, mainFrameId), b, 0644)
-	//		i++
-	//	}
-	//})
-	//
-	////remote.CallbackEvent("Network.requestIntercepted", func(params godet.Params) {
-	////	fmt.Println("Network.requestIntercepted")
-	////	fmt.Printf("%v+\n", params)
-	////	//fmt.Println("requestWillBeSent",
-	////	//	params["type"],
-	////	//	params["documentURL"],
-	////	//	params["request"].(map[string]interface{})["url"])
-	////})
-	////
-	////remote.CallbackEvent("Network.responseReceived", func(params godet.Params) {
-	////fmt.Println("Network.requestIntercepted")
-	////fmt.Printf("%v+\n", params)
-	////fmt.Println("responseReceived",
-	////	params["type"],
-	////	params["response"].(map[string]interface{})["url"])
-	////})
-	//
-	////remote.CallbackEvent("Log.entryAdded", func(params godet.Params) {
-	////	entry := params["entry"].(map[string]interface{})
-	////	fmt.Println("LOG", entry["type"], entry["level"], entry["text"])
-	////})
-	//
-	//// block loading of most images
-	////_ = remote.SetBlockedURLs("*.jpg", "*.png", "*.gif")
-	//
-	//// create new tab
-	//tab, _ := remote.NewTab("https://www.google.com")
-	//defer func() {
-	//	remote.CloseTab(tab)
-	//}()
-	//
-	//// enable event processing
-	//remote.RuntimeEvents(true)
-	//remote.NetworkEvents(true)
-	//remote.PageEvents(true)
-	//remote.DOMEvents(true)
-	//remote.LogEvents(true)
-	//
-	//// navigate in existing tab
-	//_ = remote.ActivateTab(tabs[0])
-	//
-	////remote.StartPreciseCoverage(true, true)
-	//
-	//// re-enable events when changing active tab
-	//remote.AllEvents(true) // enable all events
-	//
-	////remote.Navigate("https://www.google.com")
-	////mainFrameId, _ = remote.Navigate("http://compre.vc/v2/331179766c6")
-	////remote.Navigate("http://trk.indoleads.com/5a435e1bb6920d3422293737")
-	//
-	//mainFrameId, _ = remote.Navigate("http://trk.indoleads.com/5ad61882010d9")
-	//fmt.Printf("main frame id - %s\n", mainFrameId)
-	//
-	//// evaluate Javascript expression in existing context
-	////res, _ := remote.EvaluateWrap(`
-	////        console.log("hello from godet!")
-	////        return 42;
-	////    `)
-	////fmt.Println(res)
-	//time.Sleep(5 * time.Second)
-	//
-	//// take a screenshot
-	//_ = remote.SaveScreenshot(fmt.Sprintf("%s.png", mainFrameId), 0644, 100, true)
-	//
-	////time.Sleep(time.Second)
-	//
-	//// or save page as PDF
-	////_ = remote.SavePDF("page.pdf", 0644, godet.PortraitMode(), godet.Scale(0.5), godet.Dimensions(6.0, 2.0))
-	//
-	//// if err := remote.SetInputFiles(0, []string{"hello.txt"}); err != nil {
-	////     fmt.Println("setInputFiles", err)
-	//// }
-	//
-	////time.Sleep(5 * time.Second)
-	//
-	////remote.StopPreciseCoverage()
-	//
-	////r, err := remote.GetPreciseCoverage(true)
-	////if err != nil {
-	////	fmt.Println("error profiling", err)
-	////} else {
-	////	fmt.Println(r)
-	////}
-	//
-	////// Allow downloads
-	////_ = remote.SetDownloadBehavior(godet.AllowDownload, "/tmp/")
-	////_, _ = remote.Navigate("http://httpbin.org/response-headers?Content-Type=text/plain;%20charset=UTF-8&Content-Disposition=attachment;%20filename%3d%22test.jnlp%22")
-	////
-	////time.Sleep(time.Second)
-	////
-	////// Block downloads
-	////_ = remote.SetDownloadBehavior(godet.DenyDownload, "")
-	////_, _ = remote.Navigate("http://httpbin.org/response-headers?Content-Type=text/plain;%20charset=UTF-8&Content-Disposition=attachment;%20filename%3d%22test.jnlp%22")
-
 }
--- tracer/redirect.go
+++ tracer/redirect.go
@@ -3,29 +3,117 @@ package tracer
 import (
 	"net/http"
 	"net/url"
+	"time"
 )
 
 type redirect struct {
-	From            *url.URL       `json:"from"`
-	To              *url.URL       `json:"to"`
-	RequestHeaders  *http.Header   `json:"request_headers"`
-	ResponseHeaders *http.Header   `json:"response_headers"`
-	Cookies         []*http.Cookie `json:"cookies"`
-	//Body            []byte                 `json:"body"`
-	Status    int                    `json:"status"`
-	Initiator string                 `json:"initiator"`
-	OtherInfo map[string]interface{} `json:"other_info"`
+	From            *url.URL               `json:"from"`
+	To              *url.URL               `json:"to"`
+	RequestHeaders  *http.Header           `json:"request_headers"`
+	ResponseHeaders *http.Header           `json:"response_headers"`
+	Cookies         []*http.Cookie         `json:"cookies"`
+	Status          int                    `json:"status"`
+	Initiator       string                 `json:"initiator"`
+	OtherInfo       map[string]interface{} `json:"other_info"`
 }
 
-func NewRedirect(from, to *url.URL, requestHeaders, responseHeaders *http.Header, cookies []*http.Cookie /*body []byte,*/, status int, initiator string) *redirect {
+func NewRedirect(from, to *url.URL, requestHeaders, responseHeaders *http.Header, cookies []*http.Cookie, status int, initiator string) *redirect {
 	return &redirect{
 		From:            from,
 		To:              to,
 		RequestHeaders:  requestHeaders,
 		ResponseHeaders: responseHeaders,
 		Cookies:         cookies,
-		//Body:            body,
-		Status:    status,
-		Initiator: initiator,
+		Status:          status,
+		Initiator:       initiator,
+	}
+}
+
+type jsonRedirect struct {
+	From            string                 `json:"from"`
+	To              string                 `json:"to"`
+	RequestHeaders  map[string]string      `json:"request_headers"`
+	ResponseHeaders map[string]string      `json:"response_headers"`
+	Cookies         []*jsonCookie          `json:"cookies"`
+	Status          int                    `json:"status"`
+	Initiator       string                 `json:"initiator"`
+	OtherInfo       map[string]interface{} `json:"other_info"`
+}
+
+func NewJSONRedirects(redirects []*redirect) []*jsonRedirect {
+	var jsonRedirects []*jsonRedirect
+
+	for _, r := range redirects {
+		jsonRedirects = append(jsonRedirects, NewJSONRedirect(r))
+	}
+
+	return jsonRedirects
+}
+
+func NewJSONRedirect(r *redirect) *jsonRedirect {
+	rRequestHeaders := make(map[string]string)
+	rResponseHeaders := make(map[string]string)
+
+	for k := range *r.RequestHeaders {
+		rRequestHeaders[k] = r.RequestHeaders.Get(k)
+	}
+	for k := range *r.ResponseHeaders {
+		rResponseHeaders[k] = r.RequestHeaders.Get(k)
+	}
+
+	return &jsonRedirect{
+		From:            r.From.String(),
+		To:              r.To.String(),
+		RequestHeaders:  rRequestHeaders,
+		ResponseHeaders: rResponseHeaders,
+		Cookies:         NewJSONCookies(r.Cookies),
+		Status:          r.Status,
+		Initiator:       r.Initiator,
+		OtherInfo:       r.OtherInfo,
+	}
+}
+
+type jsonCookie struct {
+	Name  string `json:"name"`
+	Value string `json:"value"`
+
+	Path       string    `json:"path"`        // optional
+	Domain     string    `json:"domain"`      // optional
+	Expires    time.Time `json:"expires"`     // optional
+	RawExpires string    `json:"raw_expires"` // for reading cookies only
+
+	// MaxAge=0 means no 'Max-Age' attribute specified.
+	// MaxAge<0 means delete cookie now, equivalently 'Max-Age: 0'
+	// MaxAge>0 means Max-Age attribute present and given in seconds
+	MaxAge   int      `json:"max_age"`
+	Secure   bool     `json:"secure"`
+	HttpOnly bool     `json:"http_only"`
+	Raw      string   `json:"raw"`
+	Unparsed []string `json:"unparsed"` // Raw text of unparsed attribute-value pairs
+}
+
+func NewJSONCookies(cookies []*http.Cookie) []*jsonCookie {
+	var jsonCookies []*jsonCookie
+
+	for _, c := range cookies {
+		jsonCookies = append(jsonCookies, NewJSONCookie(c))
+	}
+
+	return jsonCookies
+}
+
+func NewJSONCookie(cookie *http.Cookie) *jsonCookie {
+	return &jsonCookie{
+		Name:       cookie.Name,
+		Value:      cookie.Value,
+		Path:       cookie.Path,
+		Domain:     cookie.Domain,
+		Expires:    cookie.Expires,
+		RawExpires: cookie.RawExpires,
+		MaxAge:     cookie.MaxAge,
+		Secure:     cookie.Secure,
+		HttpOnly:   cookie.HttpOnly,
+		Raw:        cookie.Raw,
+		Unparsed:   cookie.Unparsed,
 	}
 }
